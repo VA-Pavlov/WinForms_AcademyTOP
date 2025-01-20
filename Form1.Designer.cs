91110571824db558e7d9048d772b33e6a46abb2d
VA-Pavlov <vap.uspeh@gmail.com>
2025-01-20 18:34:07 +0300

    примеры

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            checkedListBox1 = new CheckedListBox();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "Вариант 1", "Вариант 2", "Вариант 3", "Вариант 4" });
+            listBox1.Location = new Point(125, 40);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 0;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "Вариант 5", "Вариант 6", "Вариант 7", "Вариант 8" });
+            checkedListBox1.Location = new Point(286, 40);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Москва", "Питер", "Амстердам" });
+            comboBox1.Location = new Point(443, 40);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(183, 197);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Удалить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(353, 309);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(276, 197);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Вывести в label";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(443, 167);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(443, 196);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 23);
+            button3.TabIndex = 7;
+            button3.Text = "добавить";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private CheckedListBox checkedListBox1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,33 @@ namespace WinForms_AcademyTOP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                checkedListBox1.Items.Remove(checkedListBox1.CheckedItems[i]);
+                i--;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label1.Text += listBox1.SelectedItem.ToString();
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                label1.Text += "\n " + item.ToString();
+            }
+            label1.Text += comboBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            checkedListBox1.Items.Add(textBox1.Text);
+            comboBox1.Items.Add(textBox1.Text);
+        }
     }
 }
